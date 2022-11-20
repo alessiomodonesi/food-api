68a47d191db02425bc6a0e00e0f7c2c33e947b7d
alessiomodonesi <alessio.modonesi@iisviolamarchesini.edu.it>
2022-11-20 01:11:08 +0100

    prove

--- api/Favourites/getArchiveFavourites.php
+++ api/Favourites/getArchiveFavourites.php
@@ -1,2 +1,3 @@
 <?php
+    echo 'silence is golden';
 ?>
--- db/connectDB.php
+++ db/connectDB.php
@@ -1,13 +1,14 @@
 <?php
-    define("DB_HOST", "localhost"); 
-    define("DB_USERNAME", "root"); 
-    define("DB_DATABASE_NAME", "sandwiches"); 
-    define("DB_PORT", "3306"); 
+    define("DB_HOST", "localhost");
+    define("DB_USERNAME", "root");
+    define("DB_DATABASE_NAME", "sandwiches");
+    define("DB_PORT", "3306");
     define("DB_PASSWORD", "");
 
-    $connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME, DB_PORT);
-    echo var_dump($connection);
+    $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME, DB_PORT);
 
     if ($conn->connect_error)
         die("Connection failed: " . $conn->connect_error);
+    else
+        echo "Connection succeeded";
 ?>
