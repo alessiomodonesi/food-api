3490a7ca0a2c9a804483aaf756c8b21ff6e31461
alessiomodonesi <alessio.modonesi@iisviolamarchesini.edu.it>
2022-11-20 14:56:10 +0100

    sistemato index.php

--- db/connectDB.php
+++ db/connectDB.php
@@ -9,4 +9,6 @@
 
     if ($conn->connect_error)
         die("Connection failed: " . $conn->connect_error);
+    else
+        die("Connection succeeded");
 ?>
--- model/testController.php
+++ model/testController.php
@@ -2,19 +2,19 @@
     require("baseController.php");
     class TestController extends BaseController
     {   
-        public function CheckIngredient() //stampa tutti gli ingredienti
+        public function CheckProduct() //stampa tutti i prodotti
         {
-            $sql = "select distinct name as 'Nome ingrediente', available_quantity as 'Quantita disponibile'
-                    from ingredient
+            $sql = "select distinct name as 'Nome prodotto', quantity as 'Quantita disponibile'
+                    from product 
                     order by ID;";
 
             $result = $this->conn->query($sql);
             $this->SendOutput($result, array('Content-Type: application/json', 'HTTP/1.1 200 OK'));
         }
-        public function CheckProduct() //stampa tutti i prodotti
+        public function CheckIngredient() //stampa tutti gli ingredienti
         {
-            $sql = "select distinct name as 'Nome prodotto', quantity as 'Quantita disponibile'
-                    from product 
+            $sql = "select distinct name as 'Nome ingrediente', available_quantity as 'Quantita disponibile'
+                    from ingredient
                     order by ID;";
 
             $result = $this->conn->query($sql);
