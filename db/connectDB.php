22a6d0d0cf4b9109a5dfae319baafb3946aa5ae5
alessiomodonesi <alessio.modonesi@iisviolamarchesini.edu.it>
2022-11-20 01:00:04 +0100

    Update connectDB.php

--- db/connectDB.php
+++ db/connectDB.php
@@ -1,14 +1,12 @@
 <?php
-    // Properties
-    define("DB_HOST", "localhost");
-    define("DB_USERNAME", "root");
-    define("DB_DATABASE_NAME", "sandwiches");
-    define("DB_PORT", "3306");
+    define("DB_HOST", "localhost"); 
+    define("DB_USERNAME", "root"); 
+    define("DB_DATABASE_NAME", "sandwiches"); 
+    define("DB_PORT", "3306"); 
     define("DB_PASSWORD", "");
 
-    //Connection
-    $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME, DB_PORT);
-    //echo print_r($conn);
+    $connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME, DB_PORT);
+    echo var_dump($connection);
 
     if ($conn->connect_error)
         die("Connection failed: " . $conn->connect_error);
