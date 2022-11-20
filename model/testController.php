2ee37d7e34b7736d6eb3f51df06436d866ade736
alessiomodonesi <alessio.modonesi@iisviolamarchesini.edu.it>
2022-11-20 01:43:56 +0100

    Update testController.php

--- model/testController.php
+++ model/testController.php
@@ -2,7 +2,7 @@
     require("baseController.php");
     class TestController extends BaseController
     {   
-        public function CheckIngredient()
+        public function CheckIngredient() //stampa tutti gli ingredienti
         {
             $sql = "select distinct name as 'Nome ingrediente', available_quantity as 'Quantita disponibile'
                     from ingredient
@@ -11,7 +11,7 @@
             $result = $this->conn->query($sql);
             $this->SendOutput($result, array('Content-Type: application/json', 'HTTP/1.1 200 OK'));
         }
-        public function CheckProduct()
+        public function CheckProduct() //stampa tutti i prodotti
         {
             $sql = "select distinct name as 'Nome prodotto', quantity as 'Quantita disponibile'
                     from product 
