9aef424573b52d66f3a81e1c3fc7c629b7a39b3f
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2022-11-26 12:56:14 +0100

    Creata api di associazione tra tag e ingrediente

---
+++ API/category 5F/tag 5F/setIngredientTag.php
@@ -0,0 +1,34 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    include_once dirname(__FILE__) . '/../../../COMMON/connect.php';
+    include_once dirname(__FILE__) . '/../../../MODEL/tag.php';
+
+    $database = new Database();
+    $db = $database->connect();
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    if(empty($data)){
+        http_response_code(400);
+        die(json_encode(array("Message" => "Bad request")));
+    }
+
+    $tag = new Tag($db);
+    
+    if(!empty($record = $tag->setIngredientTag($data->ingredient_ID, $data->tag_ID)))
+    {
+        http_response_code(201);
+        echo json_encode(array("Message"=> "Created"));
+    }
+    else
+    {
+        http_response_code(503);
+        echo json_encode(array("Message"=>'Error'));
+    }
+
+?>
