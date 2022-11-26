d88942cc195c29a782443def6d0d0cfaefae1666
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2022-11-26 12:55:18 +0100

    Creata api di eliminazione dell'associazione tra tag e ingrediente (NOTA: funzionante ma da rivedere)

---
+++ API/category 5F/tag 5F/deleteIngredientTag.php
@@ -0,0 +1,29 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once dirname(__FILE__) . '/../../../COMMON/connect.php';
+include_once dirname(__FILE__) . '/../../../MODEL/tag.php';
+
+$database = new Database();
+$db = $database->connect();
+
+if (!strpos($_SERVER["REQUEST_URI"], "tag_ID=") || !strpos($_SERVER["REQUEST_URI"], "ingredient_ID=") ) // Controlla se l'URI contiene ?ID
+{
+    http_response_code(400);
+    die(json_encode(array("Message" => "Bad request")));
+}
+
+$tag_ID = explode("tag_ID=" ,$_SERVER['REQUEST_URI'])[1]; 
+$ingredient_ID = explode("ingredient_ID=" ,$_SERVER['REQUEST_URI'])[1]; 
+$tag = new Tag($db);
+$stmt = $tag->deleteIngredientTag($ingredient_ID, $tag_ID);
+
+if ($stmt === TRUE)
+{
+    echo "delete carried out";
+}
+else {
+    echo "\n\nNo record";
+}
+?>
