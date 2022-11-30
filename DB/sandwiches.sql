2487df16c9818da41dd9131b2f9889eeaa652d9f
alessiomodonesi <alessiomodo2004@gmail.com>
2022-11-30 12:43:48 +0100

    Definitivo script

--- DB/sandwiches.sql
+++ DB/sandwiches.sql
@@ -1,338 +1,196 @@
 CREATE DATABASE sandwiches;
-USE sandwiches;
 
--- tabelle principali
+CREATE  TABLE sandwiches.allergen ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	name                 VARCHAR(50)  NOT NULL     
+ );
 
-CREATE TABLE account(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-name NVARCHAR(32) NOT NULL,
-surname NVARCHAR(32) NOT NULL,
-email NVARCHAR(64) UNIQUE NOT NULL,
-password NVARCHAR(32) NOT NULL,
-active BIT DEFAULT 1
-);
+CREATE  TABLE sandwiches.break ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	`time`               TIME  NOT NULL     
+ );
 
-CREATE TABLE category(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-name NVARCHAR(16) NOT NULL,
-iva_tax DECIMAL(10,2) NOT NULL,
-CHECK (iva_tax > 0)
-);
+CREATE  TABLE sandwiches.cart_product ( 
+	cart                 INT UNSIGNED NOT NULL,
+	product              INT UNSIGNED NOT NULL,
+	quantity             INT UNSIGNED
+ );
 
-CREATE TABLE nutritional_value(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-kcal INT NOT NULL,
-fats DECIMAL(10,2) NOT NULL,
-saturated_fats DECIMAL(10,2),
-carbohydrates DECIMAL(10,2) NOT NULL,
-sugars DECIMAL(10,2),
-proteins DECIMAL(10,2) NOT NULL,
-salt DECIMAL(10,2),
-fiber DECIMAL(10,2),
-CHECK (kcal > 0),
-CHECK (fats >= 0),
-CHECK (carbohydrates >= 0),
-CHECK (proteins >= 0)
-);
+CREATE  TABLE sandwiches.class ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	year                 INT UNSIGNED NOT NULL,
+	section              VARCHAR(1)  NOT NULL     
+ );
 
-CREATE TABLE product(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-name NVARCHAR(32) NOT NULL,
-price DECIMAL(3,2) NOT NULL,
-description NVARCHAR(128),
-category_ID INT NOT NULL,
-quantity INT NOT NULL,
-nutritional_value_ID INT NOT NULL,
-active BIT DEFAULT 1,
-CHECK (price > 0),
-CHECK (quantity > 0),
-FOREIGN KEY (category_ID) REFERENCES category(ID),
-FOREIGN KEY (nutritional_value_ID) REFERENCES nutritional_value(ID)
-);
+CREATE  TABLE sandwiches.ingredient ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	name                 VARCHAR(50)  NOT NULL,
+	description          VARCHAR(128),
+	price                DECIMAL(4,2) UNSIGNED,
+	extra                BOOLEAN NOT NULL DEFAULT (FALSE),
+	quantity             INT UNSIGNED NOT NULL
+ );
 
-CREATE TABLE ingredient(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-name NVARCHAR(32) NOT NULL,
-description NVARCHAR(128),
-available_quantity DECIMAL(10,2) NOT NULL,
-extra_ready BIT DEFAULT 0,
-active BIT DEFAULT 1,
-CHECK (available_quantity >= 0)
-);
+CREATE  TABLE sandwiches.ingredient_allergen ( 
+	ingredient           INT UNSIGNED NOT NULL,
+	allergen             INT UNSIGNED NOT NULL
+ );
 
-CREATE TABLE cart(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-user_ID INT NOT NULL,
-total_price DECIMAL(10,2) NOT NULL,
-CHECK (total_price > 0),
-FOREIGN KEY (user_ID) REFERENCES account(ID)
-);
+CREATE  TABLE sandwiches.pickup ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	name                 VARCHAR(128)  NOT NULL     
+ );
 
-CREATE TABLE status(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-description NVARCHAR(16) NOT NULL
-);
+CREATE  TABLE sandwiches.pickup_break ( 
+	pickup               INT UNSIGNED NOT NULL,
+	break                INT UNSIGNED NOT NULL     
+ );
 
-CREATE TABLE break(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-break_time TIME NOT NULL
-);
+CREATE  TABLE sandwiches.product ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	name                 VARCHAR(50)  NOT NULL,
+	price                DECIMAL(4,2) UNSIGNED NOT NULL,
+	description          VARCHAR(128),
+	quantity             INT  NOT NULL,
+	nutritional_value    INT UNSIGNED NOT NULL,
+	active               BOOLEAN  NOT NULL DEFAULT (TRUE)   
+ );
 
-CREATE TABLE pickup_point(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-description NVARCHAR(64) NOT NULL
-);
+CREATE  TABLE sandwiches.nutritional_value ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	kcal                 INT NOT NULL,
+	fats                 DECIMAL(4,2) NOT NULL,
+	saturated_fats       DECIMAL(4,2),
+	carbohydrates        DECIMAL(4,2) NOT NULL,
+	sugars               DECIMAL(4,2),
+	proteins             DECIMAL(4,2) NOT NULL,
+	fiber                DECIMAL(4,2),
+	salt                 DECIMAL(4,2)
+ );
 
-CREATE TABLE user_order(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-user_ID INT NOT NULL,
-total_price DECIMAL(10,2) NOT NULL,
-date_hour_sale DATETIME NOT NULL DEFAULT current_timestamp,
-break_ID INT NOT NULL,
-status_ID INT NOT NULL,
-pickup_ID INT NOT NULL,
-json LONGTEXT,
-CHECK (total_price > 0),
-FOREIGN KEY (user_ID) REFERENCES account(ID),
-FOREIGN KEY (break_ID) REFERENCES break(ID),
-FOREIGN KEY (status_ID) REFERENCES status(ID),
-FOREIGN KEY (pickup_ID) REFERENCES pickup_point(ID)
-);
+CREATE  TABLE sandwiches.product_ingredient ( 
+	product              INT UNSIGNED NOT NULL,
+	ingredient           INT UNSIGNED NOT NULL     
+ );
 
-CREATE TABLE catalog(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-catalog_name NVARCHAR(30) NOT NULL,
-validity_start_date DATE NOT NULL,
-validity_end_date DATE NOT NULL,
-CHECK (validity_start_date < validity_end_date)
-);
+CREATE  TABLE sandwiches.`status` ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	description          VARCHAR(50)  NOT NULL     
+ );
 
-CREATE TABLE special_offer(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-title NVARCHAR(16) NOT NULL,
-description NVARCHAR(64),
-offer_code NVARCHAR(8) NOT NULL,
-validity_start_date DATE NOT NULL,
-validity_end_date DATE NOT NULL,
-CHECK (validity_start_date < validity_end_date)
-);
+CREATE  TABLE sandwiches.tag ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	name                 VARCHAR(20)  NOT NULL     
+ );
 
-CREATE TABLE tag(
-tag_ID INT AUTO_INCREMENT PRIMARY KEY,
-tag NVARCHAR(32) NOT NULL
-);
+CREATE  TABLE sandwiches.`user` ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	name                 VARCHAR(50)  NOT NULL,
+	surname              VARCHAR(50)  NOT NULL,
+	class                INT UNSIGNED NOT NULL,
+	email                VARCHAR(128)  NOT NULL,
+	password             VARCHAR(128)  NOT NULL,
+	active               BOOLEAN  NOT NULL DEFAULT (TRUE)    
+ );
 
-CREATE TABLE class(
-ID INT AUTO_INCREMENT PRIMARY KEY,
-year_class INT NOT NULL,
-section_class NVARCHAR(1) NOT NULL
-);
+ CREATE  TABLE sandwiches.user_class (
+	user                 INT UNSIGNED NOT NULL,
+	class                INT UNSIGNED NOT NULL,
+	`year`               YEAR NOT NULL
+ );
 
--- tabelle di mezzo
+CREATE  TABLE sandwiches.cart ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	`user`               INT UNSIGNED NOT NULL     
+ );
 
-CREATE TABLE order_product(
-order_ID INT,
-product_ID INT,
-quantity INT,
-CHECK (quantity > 0),
-FOREIGN KEY (order_ID) REFERENCES user_order(ID),
-FOREIGN KEY (product_ID) REFERENCES product(ID),
-CONSTRAINT pk_order_product PRIMARY KEY (order_ID, product_ID)
-);
+CREATE  TABLE sandwiches.favourite ( 
+	`user`               INT UNSIGNED NOT NULL,
+	product              INT UNSIGNED NOT NULL,
+	created              TIMESTAMP   DEFAULT (CURRENT_TIMESTAMP)    
+ );
 
-CREATE TABLE cart_product(
-cart_ID INT,
-product_ID INT,
-quantity INT,
-CHECK (quantity > 0),
-FOREIGN KEY (cart_ID) REFERENCES cart(ID),
-FOREIGN KEY (product_ID) REFERENCES product(ID),
-CONSTRAINT pk_cart_product PRIMARY KEY (cart_ID, product_ID)
-);
+CREATE  TABLE sandwiches.offer ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	product              INT UNSIGNED NOT NULL,
+	price                DECIMAL(4,2) UNSIGNED NOT NULL,
+	expiry               TIMESTAMP  NOT NULL,
+	description          VARCHAR(128)       
+ );
 
-CREATE TABLE ingredient_tag(
-ingredient_ID INT,
-tag_ID INT,
-FOREIGN KEY (ingredient_ID) REFERENCES ingredient(ID),
-FOREIGN KEY (tag_ID) REFERENCES tag(tag_ID),
-CONSTRAINT pk_ingredient_tag PRIMARY KEY (ingredient_ID, tag_ID)
-);
+CREATE  TABLE sandwiches.`order` ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	`user`               INT UNSIGNED NOT NULL,
+	created              TIMESTAMP  NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+	pickup               INT UNSIGNED NOT NULL,
+	break                INT UNSIGNED NOT NULL,
+	`status`             INT UNSIGNED NOT NULL,
+	json                 LONGTEXT
+ );
 
-CREATE TABLE catalog_product(
-catalog_ID INT,
-product_ID INT,
-FOREIGN KEY (catalog_ID) REFERENCES catalog(ID),
-FOREIGN KEY (product_ID) REFERENCES product(ID),
-CONSTRAINT pk_catalog_product PRIMARY KEY (catalog_ID, product_ID)
-);
+CREATE  TABLE sandwiches.product_order ( 
+	product              INT UNSIGNED NOT NULL,
+	`order`              INT UNSIGNED NOT NULL
+ );
 
-CREATE TABLE offer_category(
-offer_ID INT,
-category_ID INT,
-FOREIGN KEY (offer_ID) REFERENCES special_offer(ID),
-FOREIGN KEY (category_ID) REFERENCES category(ID),
-CONSTRAINT pk_offer_category PRIMARY KEY (offer_ID, category_ID)
-);
+CREATE  TABLE sandwiches.product_tag ( 
+	product              INT UNSIGNED NOT NULL,
+	tag                  INT UNSIGNED NOT NULL    
+ );
 
-CREATE TABLE product_ingredient(
-product_ID INT,
-ingredient_ID INT,
-ingredient_quantity nvarchar(32) NOT NULL,
-product_making_notes nvarchar(128),
-CHECK (ingredient_quantity > 0),
-FOREIGN KEY (product_ID) REFERENCES product(ID),
-FOREIGN KEY (ingredient_ID) REFERENCES ingredient(ID),
-CONSTRAINT pk_product_ingredient PRIMARY KEY (product_ID, ingredient_ID)
-);
+CREATE  TABLE sandwiches.reset ( 
+	id                   INT UNSIGNED NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
+	`user`               INT UNSIGNED NOT NULL,
+	password             VARCHAR(128)  NOT NULL,
+	requested            TIMESTAMP  NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+	expires              TIMESTAMP  NOT NULL,
+	completed            BOOLEAN  NOT NULL DEFAULT (FALSE)    
+ );
 
-CREATE TABLE account_class (
-class_ID INT NOT NULL,
-account_ID INT NOT NULL,
-start_year DATE NOT NULL,
-FOREIGN KEY (class_ID) REFERENCES class(ID),
-FOREIGN KEY (account_ID) REFERENCES account(ID)
-);
+ALTER TABLE sandwiches.cart_product ADD CONSTRAINT fk_cart_product_product FOREIGN KEY ( product ) REFERENCES sandwiches.product ( id );
 
--- import
+ALTER TABLE sandwiches.cart_product ADD CONSTRAINT fk_cart_product_cart FOREIGN KEY ( cart ) REFERENCES sandwiches.cart ( id );
 
-INSERT INTO category(name, iva_tax)
-VALUES
-('panino', 0.22),
-('bevanda', 0.18),
-('piadina', 0.22);
+ALTER TABLE sandwiches.cart ADD CONSTRAINT fk_cart_user FOREIGN KEY ( `user` ) REFERENCES sandwiches.`user` ( id );
 
-INSERT INTO nutritional_value(kcal, fats, carbohydrates, proteins)
-VALUES
-(235, 25, 80, 7),
-(348, 30, 63, 6),
-(249, 17, 65, 25),
-(80, 0, 10, 1);
+ALTER TABLE sandwiches.ingredient_allergen ADD CONSTRAINT fk_ingredient_allergen_ingredient FOREIGN KEY ( ingredient ) REFERENCES sandwiches.ingredient ( id );
 
-INSERT INTO product(name, price, description, quantity, category_ID, nutritional_value_ID)
-VALUES
-('panino al prosciutto', 3, 'panino fatto col miglior prosciutto in cirolazione', 26, 1, 1),
-('panino al salame', 3, 'panino fatto col salame de me nonno', 17, 1, 2),
-('panino proteico', 3, 'panino che possono mangiare solo i veri jimbro', 15, 1, 3),
-('coca cola', 1, 'bevanda frizzante', 24, 2, 4),
-('panino col formaggio', 1.20, 'panino con il formaggio del despar', 15, 1, 2);
+ALTER TABLE sandwiches.ingredient_allergen ADD CONSTRAINT fk_ingredient_allergen_allergen FOREIGN KEY ( allergen ) REFERENCES sandwiches.allergen ( id );
 
-INSERT INTO ingredient(name, available_quantity, description)
-VALUES
-('salame', 60, 'salame de me nonno'),
-('prosciutto', 35, 'miglior prosciutto in cirolazione'),
-('pane', 80, 'pane da panino'),
-('bresaola', 40, 'we jim'),
-('formaggio', 60, 'formaggio del despar');
+ALTER TABLE sandwiches.pickup_break ADD CONSTRAINT fk_pickup_break_pickup FOREIGN KEY ( pickup ) REFERENCES sandwiches.pickup ( id );
 
-INSERT INTO product_ingredient(product_ID, ingredient_ID, ingredient_quantity, product_making_notes)
-VALUES
-(1, 3, 2, 'se è un panino è ovvio che ci sia del pane :D'),
-(2, 3, 2, 'se è un panino è ovvio che ci sia del pane :D'),
-(3, 3, 2, 'se è un panino è ovvio che ci sia del pane :D'),
-(5, 3, 2, 'se è un panino è ovvio che ci sia del pane :D'),
-(1, 2, 3, 'per panino al prosciutto'),
-(2, 1, 3, 'per panino al salame'),
-(3, 4, 4, 'per panino dei jimbro'),
-(5, 5, 3, 'formaggio del panino');
+ALTER TABLE sandwiches.pickup_break ADD CONSTRAINT fk_pickup_break_break FOREIGN KEY ( break ) REFERENCES sandwiches.break ( id );
 
-INSERT INTO account(name, surname, email, password, active)
-VALUES
-('Mattia', 'Gallo', 'mattia.gallinaro@iisviolamarchesini.edu.it', 'CA71@F', 1),
-('Mattia', 'Zanini', 'mattia.zanini@iisviolamarchesini.edu.it', 'SIUUUUU', 1),
-('Alessio', 'Modonesi', 'alessio.modonesi@iisviolamarchesini.edu.it', 'CACCIOTTI', 1),
-('Cristian', 'Mondini', 'cristian.mondini@iisviolamarchesini.edu.it', 'FORZAROMA', 1);
+ALTER TABLE sandwiches.product_ingredient ADD CONSTRAINT fk_product_ingredient_product FOREIGN KEY ( product ) REFERENCES sandwiches.product ( id );
 
-INSERT INTO class(year_class, section_class)
-VALUES
-(5, 'F'),
-(5,'E'),
-(4, 'E');
+ALTER TABLE sandwiches.product_ingredient ADD CONSTRAINT fk_product_ingredient_ingredient FOREIGN KEY ( ingredient ) REFERENCES sandwiches.ingredient ( id );
 
-INSERT INTO account_class(class_ID, account_ID, start_year)
-VALUES
-(1,1, '2022/09/13'),
-(2,3, '2021/09/15'),
-(3,2, '2022/09/13'),
-(1,4, '2021/09/15');
+ALTER TABLE sandwiches.favourite ADD CONSTRAINT fk_favourite_user FOREIGN KEY ( `user` ) REFERENCES sandwiches.`user` ( id );
 
-INSERT INTO break(break_time)
-VALUES
-('09:25'),
-('11:25');
+ALTER TABLE sandwiches.favourite ADD CONSTRAINT fk_favourite_product FOREIGN KEY ( product ) REFERENCES sandwiches.product ( id );
 
-INSERT INTO `cart`(`user_ID`, `total_price`) 
-VALUES 
-('1','7.50');
--- ('2','0.00');
+ALTER TABLE sandwiches.product_tag  ADD CONSTRAINT fk_product_tag_product FOREIGN KEY ( product ) REFERENCES sandwiches.product ( id );
 
-INSERT INTO status(description)
-VALUES
-('ordinato'),
-('pronto'),
-('annullato');
+ALTER TABLE sandwiches.product_tag  ADD CONSTRAINT fk_product_tag_tag FOREIGN KEY ( tag ) REFERENCES sandwiches.tag ( id );
 
-INSERT INTO pickup_point(description)
-VALUES
-('Settore A itis'),
-('Settore B itis');
+ALTER TABLE sandwiches.product_order  ADD CONSTRAINT fk_product_order_product FOREIGN KEY ( product ) REFERENCES sandwiches.product ( id );
 
-INSERT INTO tag(tag)
-VALUES
-('glutine'),
-('latte e derivati'),
-('nessuno'),
-('uova o prodotti con uova');
+ALTER TABLE sandwiches.product_order  ADD CONSTRAINT fk_product_order_order FOREIGN KEY ( `order` ) REFERENCES sandwiches.`order` ( id );
 
-INSERT INTO ingredient_tag(ingredient_ID, tag_ID)
-VALUES
-(1, 2),
-(3, 4),
-(3, 1),
-(3,3);
+ALTER TABLE sandwiches.reset  ADD CONSTRAINT fk_reset_user FOREIGN KEY ( `user` ) REFERENCES sandwiches.`user` ( id );
 
-INSERT INTO `ingredient_tag` (`ingredient_ID`, `tag_ID`)
-VALUES 
-('5', '2');
+ALTER TABLE sandwiches.offer  ADD CONSTRAINT fk_offer_product FOREIGN KEY ( product ) REFERENCES sandwiches.product ( id );
 
-INSERT INTO `catalog`(catalog_name, validity_start_date, validity_end_date)
-VALUES
-('boh', '2022/11/23', '2022/11/30'),
-('altro', '2021/03/02', '2022/12/14'),
-('test', '2023/01/03', '2023/04/20');
+ALTER TABLE sandwiches.`order`  ADD CONSTRAINT fk_order_user FOREIGN KEY ( `user` ) REFERENCES sandwiches.`user` ( id );
 
-INSERT INTO catalog_product(catalog_ID, product_ID)
-VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(1, 4);
+ALTER TABLE sandwiches.`order`  ADD CONSTRAINT fk_order_status FOREIGN KEY ( status ) REFERENCES sandwiches.status ( id );
 
-INSERT INTO special_offer(title, description, offer_code, validity_start_date, validity_end_date)
-VALUES
-('test1', 'esempio 1', 'AFKJ86FG', '2022/01/21', '2022/02/23'),
-('test2', 'esempio 2', 'BGHE563F', '2021/03/01', '2021/10/04');
+ALTER TABLE sandwiches.`order`  ADD CONSTRAINT fk_order_pickup FOREIGN KEY ( pickup ) REFERENCES sandwiches.pickup ( id );
 
-INSERT INTO cart_product(cart_ID, product_ID, quantity)
-VALUES
-(1, 4, 1),
-(1, 1, 2);
+ALTER TABLE sandwiches.`order`  ADD CONSTRAINT fk_order_break FOREIGN KEY ( break ) REFERENCES sandwiches.break ( id );
 
-INSERT INTO offer_category(offer_ID, category_ID)
-VALUES
-(1, 3),
-(2, 1);
+ALTER TABLE sandwiches.product  ADD CONSTRAINT fk_product_nutritional_value FOREIGN KEY ( nutritional_value ) REFERENCES sandwiches.nutritional_value ( id );
 
-INSERT INTO user_order(user_ID, total_price, date_hour_sale, break_ID, status_ID, pickup_ID)
-VALUES
-(1, 7.20, '2022/11/22 09:30:00', 1, 2, 1),
-(2, 3.20, '2022/11/22 11:10:00', 2, 3, 2),
-(3, 4.00, '2022/01/02 08:57:00', 1, 2, 1);
+ALTER TABLE sandwiches.user_class  ADD CONSTRAINT fk_user_class_user FOREIGN KEY ( `user` ) REFERENCES sandwiches.`user` ( id );
 
-INSERT INTO order_product(order_ID, product_ID, quantity)
-VALUES
-(1, 2, 2),
-(1, 5, 1),
-(2, 5, 1),
-(2, 4, 2),
-(3, 4, 1),
-(3, 2, 1);
+ALTER TABLE sandwiches.user_class  ADD CONSTRAINT fk_user_class_class FOREIGN KEY ( class ) REFERENCES sandwiches.class ( id );
