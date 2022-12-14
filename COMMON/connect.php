53790079e0372c00c574d75a5f6721cb5aa4425b
alessiomodonesi <alessiomodo2004@gmail.com>
2022-12-14 14:58:51 +0100

    Update connect.php

--- COMMON/connect.php
+++ COMMON/connect.php
@@ -1,23 +1,19 @@
 <?php
 class Database
 {
-    //credentials evomatic - richiede vpn + accesso
-    protected $server_evomatic = "192.168.100.1";
-    protected $user_evomatic = "itis";
-    protected $passwd_evomatic = "itis23K..";
-
-    //credentials claudiodressadore
-    protected $server_claudio = "claudiodressadore.net";
-    protected $user_claudio = "evomatic";
-    protected $passwd_claudio = "evomatic2022";
+    //credentials evomatic
+    protected $server = "192.168.100.1";
+    protected $user = "itis";
+    protected $passwd = "itis23K..";
+    protected $db = "smart_sandwich_5f";
 
     //credentials localhost
     protected $server_local = "localhost";
     protected $user_local = "root";
     protected $passwd_local = "";
+    protected $db_local = "sandwiches";
 
     //common credentials
-    protected $db = "sandwiches";
     protected $port = "3306";
     public $conn;
 
