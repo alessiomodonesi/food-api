0e92c3b6a7e27be5ea17c40a5976a856bfb87b32
christianmondini <christian.mondini@iisviolamarchesini.edu.it>
2023-01-21 15:14:09 +0100

    importUser.php
    qualcosa

--- API/user/importUser.php
+++ API/user/importUser.php
@@ -5,7 +5,7 @@ require("../../MODEL/user.php");
 header("Content-type: application/json; charset=UTF-8");
 
 $data = json_decode(file_get_contents("php://input"));
-print_r($data[0]);
+//print_r($data[0]);
 
 /*
 [id] => 1 
@@ -36,13 +36,13 @@ while ($row = $result->fetch_assoc()) {
 $importDataLength = count($data);
 $tableLength = count($records);
 
-//print_r($records);
+//print_r($records[0]["name"]);
 
 //IMPORTA O AGGIORNA
 for ($i = 0; $i < $importDataLength; $i++) {
     $nontrovato = 0;
     for ($j = 0; $j < $tableLength; $j++) {
-        if ($data[$i]->name == $records[$j]->name && $data[$i]->surname == $records[$j]->surname && $data[$i]->email == $records[$j]->email) {
+        if ($data[$i]->name == $records[$j]["name"] && $data[$i]->surname == $records[$j]["surname"] && $data[$i]->email == $records[$j]["email"]) {
             $update = $user->updateUser($data[$i]->name, $data[$i]->surname, $data[$i]->email, $data[$i]->password, $data[$i]->active);
             if ($update) {
                 http_response_code(201);
